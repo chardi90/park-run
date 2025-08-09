8fe8637ac40dfeb7d1cfa4cd0386681ff03cd707
chardi90 <chardi90@hotmail.com>
2025-08-09 17:46:16 +0100

    Updated parks-data with all 22 runs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,19 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <img src="/images/ch-logo-teal.png" alt="Chardi Logo" className="logo" />
+      Coded by{" "}
+      <a href="https://www.chardi.co.uk/" target="_blank" rel="noreferrer">
+        Chardi
+      </a>
+      , open-sourced on{" "}
+      <a href="https://github.com/chardi90/" target="_blank" rel="noreferrer">
+        Github
+      </a>{" "}
+      and hosted on{" "}
+      <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+        Netlify
+      </a>
+      .
     </div>
   );
 }

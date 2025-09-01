e2422fad19a40754bbe35c2e9b30852ccd3958fb
chardi90 <chardi90@hotmail.com>
2025-09-01 17:06:30 +0100

    Readjusted Header content & removed Css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./HeaderFooter.css";
 
 export default function Footer() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
-import "./HeaderFooter.css";
 import logo from "./images/ch-logo-teal.png";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <div>
-        <img src={logo} alt="Chardi Logo" className="logo" />
-      </div>
-      <div className="heading">
-        <h1>Great Manchester Parkrun Challenge</h1>
-        <h2>Run, run, run! Can you catch 'em all?</h2>
+    <div
+      className="bg-slate-800 text-white px-4 py-2"
+      style={{ height: "5vh" }}
+    >
+      <div className="flex items-center justify-between h-full">
+        <div>
+          <img src={logo} alt="Chardi Logo" style={{ height: "3vh" }} />
+        </div>
+        <h1 className="text-sm font-bold text-teal-400 truncate">
+          Great Manchester Parkrun Challenge
+        </h1>
+        <span className="text-xs text-gray-300">Run 'em all!</span>
       </div>
     </div>
   );
